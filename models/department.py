36b1bae9450f6cc72bed63497221b375c35427ae
casierrav <casierrav@unal.edu.co>
2022-11-24 09:00:46 -0500

    Basic definition of the backend models

---
+++ models/department.py
@@ -0,0 +1,32 @@
+"""
+ # Copyright: Copyright (c) 2022
+ #
+ # License
+ #
+ # Copyright (c) 2022 by Carlos Andres Sierra Virgüez.
+ # All rights reserved.
+ #
+ # This file is part of Academic #MisionTIC2022 Project Software.
+ #
+ # Academic #MisionTIC2022 Project is free software: you can redistribute it and/or modify it
+ # under the terms of the GNU General Public License as published by the Free Software Foundation,
+ # either version 3 of the License, or (at your option) any later version.
+ #
+ # Academic #MisionTIC2022 Project is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ # without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ # See the GNU General Public License for more details.
+ #
+ # You should have received a copy of the GNU General Public License along with Academic #MisionTIC2022 Project.
+ # If not, see <https://www.gnu.org/licenses/>.
+"""
+
+from models.abstract_model import AbstractModel
+
+
+class Department(AbstractModel):
+    """
+    This class represents the behavior of an academic department in the application.
+    The expected data structure for the academic department is defined as follows:
+    name (str)
+    """
+    pass
---
+++ models/student.py
@@ -0,0 +1,34 @@
+"""
+ # Copyright: Copyright (c) 2022
+ #
+ # License
+ #
+ # Copyright (c) 2022 by Carlos Andres Sierra Virgüez.
+ # All rights reserved.
+ #
+ # This file is part of Academic #MisionTIC2022 Project Software.
+ #
+ # Academic #MisionTIC2022 Project is free software: you can redistribute it and/or modify it
+ # under the terms of the GNU General Public License as published by the Free Software Foundation,
+ # either version 3 of the License, or (at your option) any later version.
+ #
+ # Academic #MisionTIC2022 Project is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ # without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ # See the GNU General Public License for more details.
+ #
+ # You should have received a copy of the GNU General Public License along with Academic #MisionTIC2022 Project.
+ # If not, see <https://www.gnu.org/licenses/>.
+"""
+
+from models.abstract_model import AbstractModel
+
+
+class Student(AbstractModel):
+    """
+    This class represents the behavior of a student in the application.
+    The expected data structure for the student is defined as follows:
+    personal_id (int)
+    name (str)
+    lastname (str)
+    """
+    pass
